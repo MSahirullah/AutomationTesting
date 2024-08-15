8631ac52b6f9ebcd7ca2b5891b2f1c2f7ee59dd4
Mohamed Sahirullah <69881158+MSahirullah@users.noreply.github.com>
2024-08-15 14:20:43 +0530

    first commit

---
+++ AETF/src/test/java/LoadUrl/LoadUrl.java
@@ -0,0 +1,35 @@
+package LoadUrl;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+public class LoadUrl {
+
+    //WebDriver Global Variable
+    WebDriver driver;
+
+    @BeforeMethod
+    public void initializeBrowser() {
+        // Create a browser instance
+        driver = new ChromeDriver();
+
+        //Load a URL in the browser
+        driver.get("https://odel.lk/");
+    }
+
+    // Perform a task
+    @Test
+    public void test_one(){
+        System.out.println("This is test one");
+    }
+
+    // Close the browser
+    @AfterMethod
+    public void closeBrowser(){
+        driver.close();
+    }
+}
+
